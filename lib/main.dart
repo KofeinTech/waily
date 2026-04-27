e88b3ac53852bd6539b0a0b1c99e9a85da22feba
kolyamriy-dev <kolyamriy@improvs.com>
2026-04-27 21:28:17 +0300

    feat(wail-11): refresh auth gate at startup before runApp

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:talker/talker.dart';
+import 'package:waily/core/env/env.dart';
+
 import 'app.dart';
 import 'core/di/injection.dart';
 import 'core/observers/app_bloc_observer.dart';
-import 'package:waily/core/env/env.dart';
+import 'core/router/auth_session_gate.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initEnv();
   configureDependencies();
   Bloc.observer = AppBlocObserver(getIt<Talker>());
+  await getIt<AuthSessionGate>().refresh();
   runApp(const App());
 }
